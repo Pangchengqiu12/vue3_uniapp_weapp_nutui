dd59dc1a21770395e8b291470b8d4cd01999bde8
li_wei <2212261902@qq,com>
2023-08-21 13:51:03 +0800

    default

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -35,18 +35,14 @@ export function checkPhone(phoneNumber) {
  * @return {string}
  */
 export function toFixed(num, fixed = 2) {
-  let numSplit = num.toString().split('.'); //将传入的数字以小点为分隔符
-  if (numSplit.length == 1) return num.toFixed(fixed); //判断分割出来的数组长度，如果是代表是整数，补零即可
-  if (numSplit[1].length <= fixed) {
-    numSplit[1] = numSplit[1].padEnd(fixed, '0'); //如果分割的数组长度大于1切小数位的长度小于保留的小数位数则补零
-    return numSplit.join('.');
+  let numSplit = num.toString().split('.');
+  if (numSplit.length == 1 || !numSplit[1][fixed] || numSplit[1][fixed] <= 4) {
+    return num.toFixed(fixed);
   }
-  if (numSplit[1][fixed] >= 5) {
-    numSplit[1] = (numSplit[1].slice(0, fixed) * 1 + 1).toString();
-    if (numSplit[1].length > fixed) {
-      numSplit[0] = (numSplit[0] * 1 + 1).toString();
-      numSplit[1] = numSplit[1].slice(1);
-    }
+  numSplit[1] = +numSplit[1].substring(0, fixed) + 1;
+  if (numSplit[1].length > fixed) {
+    numSplit[0] = +numSplit[0] + 1;
+    numSplit[1] = numSplit[1].substring(0, fixed);
   }
   return numSplit.join('.');
 }
